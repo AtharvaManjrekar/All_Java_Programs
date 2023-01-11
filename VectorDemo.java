3ed959b1f1c3d90696b38fe6876d5cd3c7d092f7
AtharvaManjrekar <113839952+AtharvaManjrekar@users.noreply.github.com>
2023-01-11 14:06:06 +0530

    Add files via upload

---
+++ VectorDemo.java
@@ -0,0 +1,13 @@
+/*
+import java.util.*;
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<String> vec = new Vector<String>();
+        vec.addElement("Atharva");
+        vec.add("Rahul");
+        vec.add("Keshav");
+
+        System.out.println(vec.hashCode());
+    }
+}
+ */
